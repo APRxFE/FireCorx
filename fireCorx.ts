8c9758d0bce88fd8f990ac10ac35f1c97e76cb87
APRxFE <aprxfe@gmail.com>
2017-02-28 14:32:34 +0900

    [update] login, logout

--- fireCorx.ts
+++ fireCorx.ts
@@ -1,3 +1,5 @@
+import * as Rx from 'rxjs'
+
 import { Injectable, NgZone } from '@angular/core'
 
 import * as firebase from 'firebase'
@@ -7,43 +9,62 @@ import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2'
 @Injectable()
 export class FireCorx {
 
-	at = firebase.database['ServerValue'].TIMESTAMP
-	auth: any = this.af.auth
-	rdb: any = this.af.database
+	at = firebase.database['ServerValue'].TIMESTAMP // Firebase TIMESTAMP
+	auth: any = this.af.auth // Auth
+	rdb: any = this.af.database // Database
 
 	root$: any = this.rdb.object('/')
-	rootRef: any = this.root$.$ref
+	rootRef: any = this.root$.$ref // Firebase Ref
 
-	private _auth$: any = null
-	isAuth: boolean = false
+	private _auth$: any
 
-	authUid: string = null
+	private _isAuth: boolean = null
+	get isAuth(){ return this._isAuth }
+	get isAuthLoad(){ return this._isAuth === null ? true : false }
+
+	private _authUid: string = null
+	get authUid(){ return this._authUid }
 
 	constructor(
 		public af: AngularFire,
 		private _zone: NgZone
 	){
 
-		// Auth 감시
-		this._auth$ = this.auth.filter(_=>!!_).subscribe(auth => {
-			auth.uid && this._zone.run(() => {
-				this.authUid = auth.uid
-				//this.router.navigate(['/'])
+		// Auth
+		this._auth$ = this.auth
+			.do(_=>_ === null && (this._isAuth = false))
+			.filter(_=>!!_).subscribe(auth => {
+				auth.uid && this._zone.run(() => {
+					this._authUid = auth.uid
+					this._isAuth = true
+				})
 			})
-		})
 
 	}
 
 	login(type: string){
-		if(type === 'google') return this._loginGoogle()
+		this.loginRn(type).subscribe(ok => {})
 	}
 
-	private _loginGoogle(){
-		this.auth.login({
-			provider: AuthProviders.Google,
-			method: AuthMethods.Redirect			
-		}).then(d => {
-		})
+	loginRn = (type: string) => {
+		if(type === 'google') return this._loginGoogleRn()
+		return Rx.Observable.of(null)
+	} 
+
+	private _loginGoogleRn = () => {
+		return Rx.Observable.fromPromise(
+			this.auth.login({ provider: AuthProviders.Google, method: AuthMethods.Redirect }) 
+		)
 	}
+
+	logout(){
+		this.logoutRn().subscribe(ok => {})
+	}
+
+	logoutRn = () => Rx.Observable.fromPromise(this.auth.logout())
+		.do(_ => {
+			this._isAuth = false
+			this._authUid = null
+		})
 
 }
